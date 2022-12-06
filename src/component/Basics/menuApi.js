5d71672118e6169f2f82479c71897d7303852b32
NishNova <nishkhamkar4112000@gmail.com>
2022-12-06 20:25:31 +0530

    Initial commit

---
+++ src/component/Basics/menuApi.js
@@ -0,0 +1,96 @@
+const Menu = [
+    {
+      id: 1,
+      image: "images/coffee.jpg",
+      name: "Coffee",
+      category: "breakfast",
+      price: "100₹",
+      description:
+        "I love Coffee realy oo yues  Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+  
+    {
+      id: 2,
+      image: "images/cheesecake.jpg",
+      name: "Cheese Cake",
+      category: "evening",
+      price: "200₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 3,
+      image: "images/pancake.jpg",
+      name: "Pancake",
+      category: "breakfast",
+      price: "150₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 4,
+      image: "../images/pizza.jpg",
+      name: "Pizza",
+      category: "lunch",
+      price: "350₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 5,
+      image: "../images/muffin.jpg",
+      name: "Muffin",
+      category: "evening",
+      price: "80₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 6,
+      image: "../images/ramen.jpg",
+      name: "Ramen",
+      category: "dinner",
+      price: "300₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 7,
+      image: "../images/milkshake.jpg",
+      name: "Oreo Milkshake",
+      category: "dinner",
+      price: "90₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 8,
+      image: "../images/burger.jpg",
+      name: "Chicken Burger",
+      category: "lunch",
+      price: "150₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",
+    },
+    {
+      id: 9,
+      image: "../images/samosa.jpg",
+      name: "Samosa",
+      category: "evening",
+      price: "50₹",
+      description:
+        "Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis, at consectetur totam voluptatibus quibusdam iusto. Accusamus quas, soluta ipsam autem eius necessitatibus fugiat in . ",  
+    },
+    // {
+    //   id: 10,
+    //   image: "",
+    //   name:,
+    //   category: ,
+    //   price: ,
+    //   description: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam assumenda expedita accusantium voluptates odit vero!"
+
+    // }
+
+  ];
+  
+  export default Menu;
